3d8906ebc524955f98e82187af44159a2d867de8
Beat Liechti <beat@softbase.tech>
2024-05-17 13:45:13 +0200

    Make i2cio module public

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,6 @@
 pub mod bme280;
 pub mod ens160;
 #[allow(dead_code)]
-mod i2cio;
+pub mod i2cio;
 
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
